c5e6374946e1f1e7c4f61fddf41d299af7d478d0
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-16 07:02:06 -0400

    Update HomeViewController.swift

--- iOS/Views/Home/HomeViewController.swift
+++ iOS/Views/Home/HomeViewController.swift
@@ -48,3 +48,71 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         button.addTarget(self, action: #selector(uploadFile), for: .touchUpInside)
         return button
     }()
+        // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupActivityIndicator()
+        loadFiles()
+        fileListTableView.delegate = self
+        fileListTableView.dataSource = self
+        fileListTableView.dragDelegate = self
+        fileListTableView.dropDelegate = self
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        filteredFileList = fileList
+    }
+
+    // MARK: - UI Setup
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        // Setup Navigation Bar
+        let navItem = UINavigationItem(title: "Files")
+        let menuButton = UIBarButtonItem(title: "\u{22ee}", style: .plain, target: self, action: #selector(showMenu))
+        let sortButton = UIBarButtonItem(title: "Sort", style: .plain, target: self, action: #selector(changeSortOrder))
+        navItem.rightBarButtonItems = [menuButton, sortButton]
+        navigationBar.setItems([navItem], animated: false)
+        
+        // Add UI elements to the view
+        view.addSubview(navigationBar)
+        view.addSubview(fileListTableView)
+        view.addSubview(activityIndicator)
+        view.addSubview(uploadButton)
+        
+        // Set up constraints with padding
+        NSLayoutConstraint.activate([
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            fileListTableView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            fileListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            fileListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            fileListTableView.bottomAnchor.constraint(equalTo: uploadButton.topAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            uploadButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            uploadButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            uploadButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            uploadButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        // Register the table view cell
+        fileListTableView.register(FileTableViewCell.self, forCellReuseIdentifier: "FileCell")
+        
+        // Add long press gesture recognizer to table view
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        fileListTableView.addGestureRecognizer(longPressRecognizer)
+    }
+
+    private func setupActivityIndicator() {
+        view.addSubview(activityIndicator)
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
