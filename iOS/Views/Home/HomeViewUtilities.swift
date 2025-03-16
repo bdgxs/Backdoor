ec7b13a7a6e84fc40a9cfee966b4fa62feb31c91
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-16 16:22:50 -0400

    Update HomeViewUtilities.swift

--- iOS/Views/Home/HomeViewUtilities.swift
+++ iOS/Views/Home/HomeViewUtilities.swift
@@ -1,7 +1,9 @@
 import UIKit
 
-extension HomeViewController {
-    func presentAlert(title: String, message: String, buttonTitle: String = "OK", handler: ((UIAlertAction) -> Void)? = nil) {
+class HomeViewUtilities {
+
+    // Method to present a simple alert
+    func presentAlert(in viewController: UIViewController, title: String, message: String, buttonTitle: String = "OK", handler: ((UIAlertAction) -> Void)? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: buttonTitle, style: .default, handler: handler)
         alert.addAction(action)
@@ -10,10 +12,11 @@ extension HomeViewController {
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         }
 
-        present(alert, animated: true, completion: nil)
+        viewController.present(alert, animated: true, completion: nil)
     }
 
-    func showInputAlert(title: String, message: String, actionTitle: String, initialText: String = "", completion: @escaping (String) -> Void) {
+    // Method to show an input alert
+    func showInputAlert(in viewController: UIViewController, title: String, message: String, actionTitle: String, initialText: String = "", completion: @escaping (String) -> Void) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addTextField { textField in
             textField.text = initialText
@@ -24,10 +27,11 @@ extension HomeViewController {
         }
         alertController.addAction(confirmAction)
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        present(alertController, animated: true, completion: nil)
+        viewController.present(alertController, animated: true, completion: nil)
     }
 
-    func handleError(_ error: Error, withTitle title: String = "Error") {
-        presentAlert(title: title, message: "An error occurred: \(error.localizedDescription)")
+    // Method to handle errors
+    func handleError(in viewController: UIViewController, error: Error, withTitle title: String = "Error") {
+        presentAlert(in: viewController, title: title, message: "An error occurred: \(error.localizedDescription)")
     }
 }
