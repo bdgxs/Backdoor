444b509829135b16a3da822a2abd239c0017cfff
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-16 07:02:38 -0400

    Update HomeViewController.swift

--- iOS/Views/Home/HomeViewController.swift
+++ iOS/Views/Home/HomeViewController.swift
@@ -116,3 +116,142 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
+        // MARK: - Load Files
+    private func loadFiles() {
+        do {
+            fileList = try fileManager.contentsOfDirectory(atPath: documentsDirectory.path)
+            sortFiles()
+            filteredFileList = fileList
+            fileListTableView.reloadData()
+        } catch {
+            presentAlert(title: "Error", message: "Failed to load files: \(error.localizedDescription)")
+        }
+    }
+
+    private func sortFiles() {
+        switch sortOrder {
+        case .name:
+            fileList.sort(by: { $0.lowercased() < $1.lowercased() })
+        case .date:
+            fileList.sort(by: { getFileDate($0) < getFileDate($1) })
+        case .size:
+            fileList.sort(by: { getFileSize($0) < getFileSize($1) })
+        }
+    }
+
+    private func getFileDate(_ fileName: String) -> Date {
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        let attributes = try? fileManager.attributesOfItem(atPath: fileURL.path)
+        return attributes?[.modificationDate] as? Date ?? Date.distantPast
+    }
+
+    private func getFileSize(_ fileName: String) -> UInt64 {
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        let attributes = try? fileManager.attributesOfItem(atPath: fileURL.path)
+        return attributes?[.size] as? UInt64 ?? 0
+    }
+
+    // MARK: - Actions
+    @objc private func showMenu() {
+        let menu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        menu.addAction(UIAlertAction(title: "Select", style: .default, handler: { _ in self.selectFiles() }))
+        menu.addAction(UIAlertAction(title: "Import", style: .default, handler: { _ in self.importFile() }))
+        menu.addAction(UIAlertAction(title: "New Folder", style: .default, handler: { _ in self.createNewFolder() }))
+        menu.addAction(UIAlertAction(title: "New File", style: .default, handler: { _ in self.createNewFile() }))
+        menu.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(menu, animated: true, completion: nil)
+    }
+
+    @objc private func changeSortOrder() {
+        let sortMenu = UIAlertController(title: "Sort By", message: nil, preferredStyle: .actionSheet)
+        sortMenu.addAction(UIAlertAction(title: "Name", style: .default, handler: { _ in self.sortOrder = .name; self.loadFiles() }))
+        sortMenu.addAction(UIAlertAction(title: "Date", style: .default, handler: { _ in self.sortOrder = .date; self.loadFiles() }))
+        sortMenu.addAction(UIAlertAction(title: "Size", style: .default, handler: { _ in self.sortOrder = .size; self.loadFiles() }))
+        sortMenu.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(sortMenu, animated: true, completion: nil)
+    }
+
+    @objc private func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let point = gesture.location(in: fileListTableView)
+            if let indexPath = fileListTableView.indexPathForRow(at: point) {
+                let fileName = filteredFileList[indexPath.row]
+                let fileURL = documentsDirectory.appendingPathComponent(fileName)
+                showFileOptions(for: fileURL)
+            }
+        }
+    }
+
+    private func showFileOptions(for fileURL: URL) {
+        let fileExtension = fileURL.pathExtension.lowercased()
+        let menu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        if fileExtension == "ipa" {
+            menu.addAction(UIAlertAction(title: "Unzip", style: .default, handler: { _ in self.unzipFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Hex Edit", style: .default, handler: { _ in self.hexEditFile(at: fileURL) }))
+        } else {
+            menu.addAction(UIAlertAction(title: "Copy", style: .default, handler: { _ in self.copyFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Move", style: .default, handler: { _ in self.moveFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Compress", style: .default, handler: { _ in self.compressFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Rename", style: .default, handler: { _ in self.renameFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Delete", style: .default, handler: { _ in self.deleteFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Share", style: .default, handler: { _ in self.shareFile(at: fileURL) }))
+        }
+        
+        menu.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(menu, animated: true, completion: nil)
+    }
+
+    private func selectFiles() {
+        // Implement select files functionality
+    }
+    
+    @objc private func uploadFile() {
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data], asCopy: true)
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .formSheet
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    private func importFile() {
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data], asCopy: true)
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .formSheet
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    private func createNewFolder() {
+        let alertController = UIAlertController(title: "New Folder", message: "Enter folder name", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Folder name"
+        }
+        let createAction = UIAlertAction(title: "Create", style: .default) { _ in
+            guard let folderName = alertController.textFields?.first?.text else { return }
+            let folderURL = self.documentsDirectory.appendingPathComponent(folderName)
+            do {
+                try self.fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
+                self.loadFiles()
+            } catch {
+                self.presentAlert(title: "Error", message: "Failed to create folder: \(error.localizedDescription)")
+            }
+        }
+        alertController.addAction(createAction)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func createNewFile() {
+        let alertController = UIAlertController(title: "New File", message: "Enter file name", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "File name"
+        }
+        let createAction = UIAlertAction(title: "Create", style: .default) { _ in
+            guard let fileName = alertController.textFields?.first?.text else { return }
+            let fileURL = self.documentsDirectory.appendingPathComponent(fileName)
+            self.fileManager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
+            self.loadFiles()
+        }
+        alertController.addAction(createAction)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
