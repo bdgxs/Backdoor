c25b016515eed67681f83907804e0402f886167a
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-15 22:28:33 -0400

    Update HomeViewController.swift

--- iOS/Views/Home/HomeViewController.swift
+++ iOS/Views/Home/HomeViewController.swift
@@ -4,82 +4,22 @@ import ZIPFoundation
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIDocumentPickerDelegate {
     
     // MARK: - Properties
-    private var ipaPath: String = ""
     private var fileList: [String] = []
     private let fileManager = FileManager.default
 
     // MARK: - UI Elements
-    private let selectIPAButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Select IPA", for: .normal)
-        button.addTarget(self, action: #selector(selectIPAButtonTapped), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+    private let navigationBar: UINavigationBar = {
+        let navBar = UINavigationBar()
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        return navBar
     }()
-
-    private let listFilesButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("List Files", for: .normal)
-        button.addTarget(self, action: #selector(listFilesButtonTapped), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
+    
     private let fileListTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
-
-    // Additional buttons for file operations
-    private let shareButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Share File", for: .normal)
-        button.addTarget(self, action: #selector(shareFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let copyButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Copy File", for: .normal)
-        button.addTarget(self, action: #selector(copyFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let moveButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Move File", for: .normal)
-        button.addTarget(self, action: #selector(moveFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let compressButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Compress File", for: .normal)
-        button.addTarget(self, action: #selector(compressFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let renameButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Rename File", for: .normal)
-        button.addTarget(self, action: #selector(renameFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
-    private let deleteButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Delete File", for: .normal)
-        button.addTarget(self, action: #selector(deleteFile), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -91,138 +31,137 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     // MARK: - UI Setup
     private func setupUI() {
         view.backgroundColor = .white
-
+        
+        // Setup Navigation Bar
+        let navItem = UINavigationItem(title: "Files")
+        let menuButton = UIBarButtonItem(title: "⋮", style: .plain, target: self, action: #selector(showMenu))
+        navItem.rightBarButtonItem = menuButton
+        navigationBar.setItems([navItem], animated: false)
+        
         // Add UI elements to the view
-        view.addSubview(selectIPAButton)
-        view.addSubview(listFilesButton)
+        view.addSubview(navigationBar)
         view.addSubview(fileListTableView)
-        view.addSubview(shareButton)
-        view.addSubview(copyButton)
-        view.addSubview(moveButton)
-        view.addSubview(compressButton)
-        view.addSubview(renameButton)
-        view.addSubview(deleteButton)
-
+        
         // Set up constraints
         NSLayoutConstraint.activate([
-            selectIPAButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            selectIPAButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            listFilesButton.topAnchor.constraint(equalTo: selectIPAButton.bottomAnchor, constant: 20),
-            listFilesButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            shareButton.topAnchor.constraint(equalTo: listFilesButton.bottomAnchor, constant: 20),
-            shareButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            copyButton.topAnchor.constraint(equalTo: shareButton.bottomAnchor, constant: 20),
-            copyButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            moveButton.topAnchor.constraint(equalTo: copyButton.bottomAnchor, constant: 20),
-            moveButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            compressButton.topAnchor.constraint(equalTo: moveButton.bottomAnchor, constant: 20),
-            compressButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            renameButton.topAnchor.constraint(equalTo: compressButton.bottomAnchor, constant: 20),
-            renameButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            deleteButton.topAnchor.constraint(equalTo: renameButton.bottomAnchor, constant: 20),
-            deleteButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-
-            fileListTableView.topAnchor.constraint(equalTo: deleteButton.bottomAnchor, constant: 20),
-            fileListTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            fileListTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            fileListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+            navigationBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            fileListTableView.topAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            fileListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            fileListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            fileListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-
+        
         // Register the table view cell
         fileListTableView.register(UITableViewCell.self, forCellReuseIdentifier: "FileCell")
+        
+        // Add long press gesture recognizer to table view
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+        fileListTableView.addGestureRecognizer(longPressRecognizer)
     }
 
     // MARK: - Actions
-    @objc private func selectIPAButtonTapped() {
+    @objc private func showMenu() {
+        let menu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        menu.addAction(UIAlertAction(title: "Select", style: .default, handler: { _ in self.selectFiles() }))
+        menu.addAction(UIAlertAction(title: "Import", style: .default, handler: { _ in self.importFile() }))
+        menu.addAction(UIAlertAction(title: "New Folder", style: .default, handler: { _ in self.createNewFolder() }))
+        menu.addAction(UIAlertAction(title: "New File", style: .default, handler: { _ in self.createNewFile() }))
+        menu.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(menu, animated: true, completion: nil)
+    }
+
+    @objc private func handleLongPress(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let point = gesture.location(in: fileListTableView)
+            if let indexPath = fileListTableView.indexPathForRow(at: point) {
+                let fileName = fileList[indexPath.row]
+                let fileURL = URL(fileURLWithPath: fileName)
+                showFileOptions(for: fileURL)
+            }
+        }
+    }
+    
+    private func showFileOptions(for fileURL: URL) {
+        let fileExtension = fileURL.pathExtension.lowercased()
+        let menu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        if fileExtension == "ipa" {
+            menu.addAction(UIAlertAction(title: "Unzip", style: .default, handler: { _ in self.unzipFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Hex Edit", style: .default, handler: { _ in self.hexEditFile(at: fileURL) }))
+        } else {
+            menu.addAction(UIAlertAction(title: "Copy", style: .default, handler: { _ in self.copyFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Move", style: .default, handler: { _ in self.moveFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Compress", style: .default, handler: { _ in self.compressFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Rename", style: .default, handler: { _ in self.renameFile(at: fileURL) }))
+            menu.addAction(UIAlertAction(title: "Delete", style: .default, handler: { _ in self.deleteFile(at: fileURL) }))
+        }
+        
+        menu.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(menu, animated: true, completion: nil)
+    }
+
+    private func selectFiles() {
+        // Implement select files functionality
+    }
+    
+    private func importFile() {
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data], asCopy: true)
         documentPicker.delegate = self
         documentPicker.modalPresentationStyle = .formSheet
         present(documentPicker, animated: true, completion: nil)
     }
-
-    @objc private func listFilesButtonTapped() {
-        listFiles()
+    
+    private func createNewFolder() {
+        // Implement create new folder functionality
     }
-
-    @objc private func shareFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
-        let activityVC = UIActivityViewController(activityItems: [fileURL], applicationActivities: nil)
-        present(activityVC, animated: true, completion: nil)
+    
+    private func createNewFile() {
+        // Implement create new file functionality
     }
-
-    @objc private func copyFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
+    
+    private func copyFile(at fileURL: URL) {
         let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent("Copy_\(fileURL.lastPathComponent)")
         do {
-            try fileManager.copyItem(at: fileURL, to: destinationURL)
+            try FileOperations.copyFile(at: fileURL, to: destinationURL)
             print("File copied to \(destinationURL.path)")
         } catch {
             print("Copy failed with error: \(error)")
         }
     }
-
-    @objc private func moveFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
+    
+    private func moveFile(at fileURL: URL) {
         let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent("Moved_\(fileURL.lastPathComponent)")
         do {
-            try fileManager.moveItem(at: fileURL, to: destinationURL)
+            try FileOperations.moveFile(at: fileURL, to: destinationURL)
             print("File moved to \(destinationURL.path)")
         } catch {
             print("Move failed with error: \(error)")
         }
     }
-
-    @objc private func compressFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
+    
+    private func compressFile(at fileURL: URL) {
         let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent("\(fileURL.lastPathComponent).zip")
         do {
-            let archive = try Archive(url: destinationURL, accessMode: .create)
-            try archive.addEntry(with: fileURL.lastPathComponent, relativeTo: fileURL.deletingLastPathComponent())
+            try FileOperations.compressFile(at: fileURL, to: destinationURL)
             print("File compressed to \(destinationURL.path)")
         } catch {
             print("Compression failed with error: \(error)")
         }
     }
-
-    @objc private func renameFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
+    
+    private func renameFile(at fileURL: URL) {
         let alertController = UIAlertController(title: "Rename File", message: "Enter new file name", preferredStyle: .alert)
         alertController.addTextField { textField in
             textField.text = fileURL.lastPathComponent
         }
         let renameAction = UIAlertAction(title: "Rename", style: .default) { _ in
             guard let newName = alertController.textFields?.first?.text else { return }
-            let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent(newName)
             do {
-                try self.fileManager.moveItem(at: fileURL, to: destinationURL)
-                print("File renamed to \(destinationURL.path)")
+                try FileOperations.renameFile(at: fileURL, to: newName)
+                print("File renamed to \(newName)")
             } catch {
                 print("Rename failed with error: \(error)")
             }
@@ -231,66 +170,38 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
-
-    @objc private func deleteFile() {
-        guard !ipaPath.isEmpty else {
-            print("Please select a file first.")
-            return
-        }
-        let fileURL = URL(fileURLWithPath: ipaPath)
+    
+    private func deleteFile(at fileURL: URL) {
         do {
-            try fileManager.removeItem(at: fileURL)
+            try FileOperations.deleteFile(at: fileURL)
             print("File deleted")
         } catch {
             print("Delete failed with error: \(error)")
         }
     }
-
-    // MARK: - ZIP Handling
-    private func listFiles() {
-        guard !ipaPath.isEmpty else {
-            print("Please select an IPA file first.")
-            return
-        }
-
+    
+    private func unzipFile(at fileURL: URL) {
+        let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent("extracted")
         do {
-            let zipFilePath = URL(fileURLWithPath: ipaPath)
-            let destinationURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("extracted")
-
-            // Check if destination directory exists, create if necessary
-            if !fileManager.fileExists(atPath: destinationURL.path) {
-                try fileManager.createDirectory(at: destinationURL, withIntermediateDirectories: true, attributes: nil)
-            }
-
-            let archive = try Archive(url: zipFilePath, accessMode: .read)
-
-            for entry in archive {
-                var destination = destinationURL.appendingPathComponent(entry.path)
-                if entry.type == .directory {
-                    try fileManager.createDirectory(at: destination, withIntermediateDirectories: true, attributes: nil)
-                } else {
-                    try archive.extract(entry, to: destination)
-                }
-                print("Extracted \(entry.path)")
-            }
-
-            // List files after extraction
-            let contents = try fileManager.contentsOfDirectory(atPath: destinationURL.path)
-            fileList = contents
-            fileListTableView.reloadData() // Reload the table view to display the files
-
+            try FileOperations.unzipFile(at: fileURL, to: destinationURL)
+            print("File unzipped to \(destinationURL.path)")
         } catch {
-            print("Extraction failed with error: \(error)")
+            print("Unzip failed with error: \(error)")
         }
     }
-
+    
+    private func hexEditFile(at fileURL: URL) {
+        FileOperations.hexEditFile(at: fileURL)
+        // Implement hex edit functionality
+    }
+    
     // MARK: - UIDocumentPickerViewControllerDelegate
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let selectedFileURL = urls.first else {
             return
         }
-        ipaPath = selectedFileURL.path
-        print("Selected IPA: \(ipaPath)")
+        // Handle file import
+        print("Selected file: \(selectedFileURL.path)")
     }
 
     // MARK: - UITableViewDelegate, UITableViewDataSource
@@ -306,7 +217,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let fileName = fileList[indexPath.row]
-        let fileURL = URL(fileURLWithPath: ipaPath).appendingPathComponent(fileName)
+        let fileURL = URL(fileURLWithPath: fileName)
         openFile(fileURL)
     }
 
