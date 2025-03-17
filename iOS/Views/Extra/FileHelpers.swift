86d5104717cf15594075f66969567665dfdea1e3
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-17 08:37:36 -0400

    Delete iOS/Views/Extra/FileHelpers.swift

--- iOS/Views/Extra/FileHelpers.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public struct FileHelpers {
-    /// Returns the URL for the app's Documents directory.
-    public static func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-}
