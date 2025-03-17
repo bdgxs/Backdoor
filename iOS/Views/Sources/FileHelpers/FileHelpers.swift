30f4c9506c79900294822c227477665fcf086f06
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-17 08:40:04 -0400

    Create FileHelpers.swift

---
+++ iOS/Views/Sources/FileHelpers/FileHelpers.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct FileHelpers {
+    /// Returns the URL for the app's Documents directory.
+    public static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
