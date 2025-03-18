365f02111cc14eb6ccb1b746d24a221cc407f7fb
BDGHubNoKey <donothackmyemailin2023@gmail.com>
2025-03-18 09:38:22 -0400

    Update feather-Bridging-Header.h

--- Shared/Magic/feather-Bridging-Header.h
+++ Shared/Magic/feather-Bridging-Header.h
@@ -1,9 +1,11 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+    //  Use this file to import your target's public headers that you would like to expose to Swift.
+    //
 
-#include "UISheetPresentationControllerDetent+Private.h"
-#include "LSApplicationWorkspace.h"
+    #include "UISheetPresentationControllerDetent+Private.h"
+    #include "LSApplicationWorkspace.h"
 
-#include "zsign.hpp"
-#include "openssl_tools.hpp"
+    #include "zsign.hpp"
+    #include "openssl_tools.hpp"
+
+    #import "CPUXLib.h" // Added this line
