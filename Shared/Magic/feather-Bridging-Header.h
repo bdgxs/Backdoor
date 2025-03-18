83fbfdc98becaf1dc4878da14b0640096efb53cf
bdgxs <jojog1010@hotmail.com>
2025-03-18 10:18:59 -0400

    Update feather-Bridging-Header.h

--- Shared/Magic/feather-Bridging-Header.h
+++ Shared/Magic/feather-Bridging-Header.h
@@ -1,11 +1,11 @@
 //
-    //  Use this file to import your target's public headers that you would like to expose to Swift.
-    //
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
 
-    #include "UISheetPresentationControllerDetent+Private.h"
-    #include "LSApplicationWorkspace.h"
+#include "UISheetPresentationControllerDetent+Private.h"
+#include "LSApplicationWorkspace.h"
 
-    #include "zsign.hpp"
-    #include "openssl_tools.hpp"
+#include "zsign.hpp"
+#include "openssl_tools.hpp"
 
-    #import "CPUXLib.h" // Added this line
+#import "Shared/CPU-X/CPUXLib.h" // Updated this line to include the relative path
