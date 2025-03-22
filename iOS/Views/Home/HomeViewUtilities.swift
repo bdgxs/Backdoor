2657aee52f3305915126702f294681d153f39bb6
bdgxs <jojog1010@hotmail.com>
2025-03-21 21:30:35 -0400

    Update HomeViewUtilities.swift

--- iOS/Views/Home/HomeViewUtilities.swift
+++ iOS/Views/Home/HomeViewUtilities.swift
@@ -33,6 +33,7 @@ struct AlertConfig {
     let completionHandler: (() -> Void)?
 }
 
+/// Structure to encapsulate alert action configurations.
 struct AlertActionConfig {
     let title: String?
     let style: UIAlertAction.Style
@@ -73,116 +74,73 @@ class HomeViewUtilities {
                 logger.error("Unknown error: \(underlyingError.localizedDescription)")
             default:
                 message = error.localizedDescription
-            }
-        } else {
-            message = error.localizedDescription
-            logger.error("Unexpected error: \(error.localizedDescription)")
         }
-
-        // Present alert on main thread
-        DispatchQueue.main.async {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            viewController.present(alert, animated: true, completion: nil)
-        }
+    } else {
+        message = error.localizedDescription
+        logger.error("Unexpected error: \(error.localizedDescription)")
     }
+
+    // Present alert on main thread
+    DispatchQueue.main.async {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
 
     // MARK: - Alert Presentation
 
-    /// Presents a basic alert using the provided configuration.
-    ///
-    /// - Parameters:
-    ///   - config: The alert configuration.
-    ///   - viewController: The view controller to present the alert in.
-    func showAlert(config: AlertConfig, in viewController: UIViewController) {
-        let alert = UIAlertController(title: config.title, message: config.message, preferredStyle: config.style)
+    extension UIViewController {
+        /// Presents a UIAlertController with the given configuration.
+        ///
+        /// - Parameter config: The configuration for the alert.
+        func presentAlert(config: AlertConfig) {
+            let alert = UIAlertController(title: config.title, message: config.message, style: config.style)
 
-        for (index, actionConfig) in config.actions.enumerated() {
-            let action = UIAlertAction(title: actionConfig.title, style: actionConfig.style) { _ in
-                actionConfig.handler?()
+            if let preferredActionIndex = config.preferredAction, preferredActionIndex < config.actions.count {
+                alert.preferredAction = alert.actions[preferredActionIndex]
             }
-            alert.addAction(action)
-            if let preferredIndex = config.preferredAction, preferredIndex == index {
-                alert.preferredAction = alert.actions[preferredIndex]
+
+            for actionConfig in config.actions {
+                let action = UIAlertAction(title: actionConfig.title, style: actionConfig.style) { _ in
+                    actionConfig.handler?()
+                }
+                alert.addAction(action)
             }
-        }
 
-        DispatchQueue.main.async {
-            viewController.present(alert, animated: true, completion: config.completionHandler)
-        }
-    }
-
-    /// Presents a confirmation alert with "OK" and "Cancel" actions.
-    ///
-    /// - Parameters:
-    ///   - title: The title for the alert.
-    ///   - message: The message for the alert.
-    ///   - okHandler: Handler to be executed when the "OK" action is tapped.
-    ///   - cancelHandler: Handler to be executed when the "Cancel" action is tapped.
-    ///   - viewController: The view controller to present the alert in.
-    func showConfirmationAlert(title: String?, message: String?, okHandler: (() -> Void)?, cancelHandler: (() -> Void)?, in viewController: UIViewController) {
-        let okAction = AlertActionConfig(title: "OK", style: .default, handler: okHandler)
-        let cancelAction = AlertActionConfig(title: "Cancel", style: .cancel, handler: cancelHandler)
-        let config = AlertConfig(title: title, message: message, style: .alert, actions: [okAction, cancelAction], preferredAction: nil, completionHandler: nil)
-        showAlert(config: config, in: viewController)
-    }
-
-    /// Presents an alert with a text field for user input.
-    ///
-    /// - Parameters:
-    ///   - title: The title for the alert.
-    ///   - message: The message for the alert.
-    ///   - textFieldHandler: Handler to configure the text field.
-    ///   - okHandler: Handler to be executed when the "OK" action is tapped, with the text field's text.
-    ///   - cancelHandler: Handler to be executed when the "Cancel" action is tapped.
-    ///   - viewController: The view controller to present the alert in.
-    func showInputAlert(title: String?, message: String?, textFieldHandler: ((UITextField) -> Void)?, okHandler: ((String?) -> Void)?, cancelHandler: (() -> Void)?, in viewController: UIViewController) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addTextField(configurationHandler: textFieldHandler)
-        
-        let okAction = AlertActionConfig(title: "OK", style: .default) {
-            let textField = alert.textFields?.first
-            okHandler?(textField?.text)
-        }
-        let cancelAction = AlertActionConfig(title: "Cancel", style: .cancel, handler: cancelHandler)
-        let config = AlertConfig(title: title, message: message, style: .alert, actions: [okAction, cancelAction], preferredAction: nil, completionHandler: nil)
-
-        for actionConfig in config.actions {
-            let action = UIAlertAction(title: actionConfig.title, style: actionConfig.style) { _ in
-                actionConfig.handler?()
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: config.completionHandler)
             }
-            alert.addAction(action)
-        }
-
-        DispatchQueue.main.async {
-            viewController.present(alert, animated: true, completion: config.completionHandler)
         }
     }
 
     // MARK: - Haptic Feedback
 
-    /// Generates haptic feedback using UIImpactFeedbackGenerator.
-    ///
-    /// - Parameter style: The style of the impact feedback.
-    func generateHapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
-        let generator = UIImpactFeedbackGenerator(style: style)
-        generator.prepare()
-        generator.impactOccurred()
+    /// Haptic feedback utility class.
+    class HapticFeedbackGenerator {
+        
+        /// Generates haptic feedback using UIImpactFeedbackGenerator.
+        ///
+        /// - Parameter style: The style of the impact feedback.
+        static func generateHapticFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+            let generator = UIImpactFeedbackGenerator(style: style)
+            generator.prepare()
+            generator.impactOccurred()
+        }
+        
+        /// Generates haptic feedback using UINotificationFeedbackGenerator.
+        ///
+        /// - Parameter type: The type of the notification feedback.
+        static func generateNotificationFeedback(type: UINotificationFeedbackGenerator.FeedbackType) {
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+        
+        /// Generates haptic feedback using UISelectionFeedbackGenerator.
+        static func generateSelectionFeedback() {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
     }
-
-    /// Generates haptic feedback using UINotificationFeedbackGenerator.
-    ///
-    /// - Parameter type: The type of the notification feedback.
-    func generateNotificationFeedback(type: UINotificationFeedbackGenerator.FeedbackType) {
-        let generator = UINotificationFeedbackGenerator()
-        generator.prepare()
-        generator.notificationOccurred(type)
-    }
-
-    /// Generates haptic feedback using UISelectionFeedbackGenerator.
-    func generateSelectionFeedback() {
-        let generator = UISelectionFeedbackGenerator()
-        generator.prepare()
-        generator.selectionChanged()
-    }
-}
