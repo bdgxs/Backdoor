f27a2e643eda2d595a516f1c75e6536da8388261
bdgxs <jojog1010@hotmail.com>
2025-03-21 21:31:52 -0400

    Update HomeViewTableHandlers.swift

--- iOS/Views/Home/HomeViewTableHandlers.swift
+++ iOS/Views/Home/HomeViewTableHandlers.swift
@@ -1,25 +1,69 @@
 import UIKit
- 
 
- extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-  return searchController.isActive ? filteredFileList.count : fileList.count
-  }
- 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchController.isActive ? filteredFileList.count : fileList.count
+    }
 
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-  let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for: indexPath) as! FileTableViewCell
-  let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
-  let fileURL = documentsDirectory.appendingPathComponent(fileName)
-  let file = File(url: fileURL)
-  cell.configure(with: file)
-  return cell
-  }
- 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for: indexPath) as! FileTableViewCell
+        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        let file = File(url: fileURL)
+        cell.configure(with: file)
+        return cell
+    }
 
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-  let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
-  let fileURL = documentsDirectory.appendingPathComponent(fileName)
-  showFileOptions(for: fileURL)
-  }
- }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        showFileOptions(for: fileURL)
+    }
+}
+
+extension HomeViewController: UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        let itemProvider = NSItemProvider(contentsOf: fileURL)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = fileName
+        return [dragItem]
+    }
+
+    func tableView(_ tableView: UITableView, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: URL.self)
+    }
+}
+
+extension HomeViewController: UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        let destinationIndexPath: IndexPath
+
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
+        } else {
+            let section = tableView.numberOfSections - 1
+            let row = tableView.numberOfRows(inSection: section)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+
+        coordinator.items.forEach { dropItem in
+            dropItem.itemProvider.loadObject(ofClass: URL.self) { [weak self] (object, error) in
+                if let url = object as? URL {
+                    let destinationURL = self?.documentsDirectory.appendingPathComponent(url.lastPathComponent)
+
+                    do {
+                        try self?.fileManager.moveItem(at: url, to: destinationURL!)
+                    } catch {
+                        print("Error dropping file: \(error)")
+                    }
+
+                    DispatchQueue.main.async {
+                        self?.loadFiles()
+                    }
+                }
+            }
+        }
+    }
+}
