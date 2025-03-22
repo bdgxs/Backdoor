3dcbda6f202bcaf0dcebcd2e81f622312f6005df
bdgxs <jojog1010@hotmail.com>
2025-03-21 21:07:46 -0400

    Update HomeViewFileHandlers.swift

--- iOS/Views/Home/HomeViewFileHandlers.swift
+++ iOS/Views/Home/HomeViewFileHandlers.swift
@@ -31,69 +31,22 @@ class HomeViewFileHandlers {
     func createNewFolder(viewController: FileHandlingDelegate, folderName: String, completion: @escaping (Result<URL, Error>) -> Void) {
         let folderURL = viewController.documentsDirectory.appendingPathComponent(folderName)
         do {
-            try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true, attributes: nil)
-            viewController.loadFiles()
+            try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: false, attributes: nil)
             completion(.success(folderURL))
         } catch {
-            utilities.handleError(in: viewController as! UIViewController, error: error, withTitle: "Creating Folder")
             completion(.failure(error))
         }
     }
 
     func createNewFile(viewController: FileHandlingDelegate, fileName: String, completion: @escaping (Result<URL, Error>) -> Void) {
         let fileURL = viewController.documentsDirectory.appendingPathComponent(fileName)
-        if !fileManager.fileExists(atPath: fileURL.path) {
-            fileManager.createFile(atPath: fileURL.path, contents: nil, attributes: nil)
-            viewController.loadFiles()
+        let fileContent = "" // Or any default content you want
+        do {
+            try fileContent.write(to: fileURL, atomically: true, encoding: .utf8)
             completion(.success(fileURL))
-        } else {
-            let error = NSError(domain: "FileExists", code: 1, userInfo: [NSLocalizedDescriptionKey: "File already exists"])
-            utilities.handleError(in: viewController as! UIViewController, error: error, withTitle: "Creating File")
+        } catch {
             completion(.failure(error))
         }
-    }
-
-    func renameFile(viewController: FileHandlingDelegate, fileURL: URL, newName: String, completion: @escaping (Result<URL, Error>) -> Void) {
-        let destinationURL = fileURL.deletingLastPathComponent().appendingPathComponent(newName)
-        viewController.activityIndicator.startAnimating()
-        let workItem = DispatchWorkItem {
-            do {
-                try self.fileManager.moveItem(at: fileURL, to: destinationURL)
-                DispatchQueue.main.async {
-                    viewController.activityIndicator.stopAnimating()
-                    viewController.loadFiles()
-                    completion(.success(destinationURL))
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    viewController.activityIndicator.stopAnimating()
-                    self.utilities.handleError(in: viewController as! UIViewController, error: error, withTitle: "Renaming File")
-                    completion(.failure(error))
-                }
-            }
-        }
-        DispatchQueue.global().async(execute: workItem)
-    }
-
-    func deleteFile(viewController: FileHandlingDelegate, fileURL: URL, completion: @escaping (Result<Void, Error>) -> Void) {
-        viewController.activityIndicator.startAnimating()
-        let workItem = DispatchWorkItem {
-            do {
-                try self.fileManager.removeItem(at: fileURL)
-                DispatchQueue.main.async {
-                    viewController.activityIndicator.stopAnimating()
-                    viewController.loadFiles()
-                    completion(.success(()))
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    viewController.activityIndicator.stopAnimating()
-                    self.utilities.handleError(in: viewController as! UIViewController, error: error, withTitle: "Deleting File")
-                    completion(.failure(error))
-                }
-            }
-        }
-        DispatchQueue.global().async(execute: workItem)
     }
 
     func unzipFile(viewController: FileHandlingDelegate, fileURL: URL, destinationName: String, progressHandler: ((Double) -> Void)? = nil, completion: @escaping (Result<URL, Error>) -> Void) {
