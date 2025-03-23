90d511666fcf699531b8f64296984d3453c5307c
bdgxs <jojog1010@hotmail.com>
2025-03-23 18:19:11 -0400

    Update HomeViewController.swift

--- iOS/Views/Home/HomeViewController.swift
+++ iOS/Views/Home/HomeViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import ZIPFoundation
 
-class HomeViewController: UIViewController, UISearchResultsUpdating, UIDocumentPickerDelegate, FileHandlingDelegate, UITableViewDelegate, UITableViewDataSource, UITableViewDragDelegate, UITableViewDropDelegate {
+class HomeViewController: UIViewController, UISearchResultsUpdating, UIDocumentPickerDelegate, FileHandlingDelegate {
     
     // MARK: - Properties
     var fileList: [String] = []
@@ -282,85 +282,6 @@ class HomeViewController: UIViewController, UISearchResultsUpdating, UIDocumentP
     // MARK: - FileHandlingDelegate
     override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
         super.present(viewControllerToPresent, animated: flag, completion: completion)
-    }
-    
-    // MARK: - UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchController.isActive ? filteredFileList.count : fileList.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for: indexPath) as? FileTableViewCell else {
-            fatalError("Unable to dequeue FileTableViewCell")
-        }
-        
-        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
-        let fileURL = documentsDirectory.appendingPathComponent(fileName)
-        let file = File(url: fileURL)
-        cell.configure(with: file)
-        return cell
-    }
-
-    // MARK: - UITableViewDelegate
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
-        let fileURL = documentsDirectory.appendingPathComponent(fileName)
-        showFileOptions(for: fileURL)
-    }
-
-    // MARK: - UITableViewDragDelegate
-    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-        let fileName = searchController.isActive ? filteredFileList[indexPath.row] : fileList[indexPath.row]
-        let fileURL = documentsDirectory.appendingPathComponent(fileName)
-        guard let itemProvider = NSItemProvider(contentsOf: fileURL) else {
-            return [] // Return empty array if item provider creation fails
-        }
-        let dragItem = UIDragItem(itemProvider: itemProvider)
-        dragItem.localObject = fileName
-        return [dragItem]
-    }
-
-    func tableView(_ tableView: UITableView, canHandle session: UIDropSession) -> Bool {
-        return session.canLoadObjects(ofClass: URL.self)
-    }
-
-    // MARK: - UITableViewDropDelegate
-    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
-        let destinationIndexPath: IndexPath
-        if let indexPath = coordinator.destinationIndexPath {
-            destinationIndexPath = indexPath
-        } else {
-            let section = tableView.numberOfSections - 1
-            let row = tableView.numberOfRows(inSection: section)
-            destinationIndexPath = IndexPath(row: row, section: section)
-        }
-
-        coordinator.items.forEach { dropItem in
-            dropItem.itemProvider.loadObject(ofClass: URL.self) { [weak self] (object, error) in
-                guard let self = self else { return }
-                if let url = object as? URL {
-                    let destinationURL = self.documentsDirectory.appendingPathComponent(url.lastPathComponent)
-                    do {
-                        try self.fileManager.moveItem(at: url, to: destinationURL)
-                        DispatchQueue.main.async {
-                            self.loadFiles()
-                            HapticFeedbackGenerator.generateNotificationFeedback(type: .success)
-                        }
-                    } catch {
-                        print("Error dropping file: \(error)")
-                        DispatchQueue.main.async {
-                            let alert = UIAlertController(
-                                title: "Error",
-                                message: "Failed to move file: \(error.localizedDescription)",
-                                preferredStyle: .alert
-                            )
-                            alert.addAction(UIAlertAction(title: "OK", style: .default))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                    }
-                }
-            }
-        }
     }
     
     // MARK: - UIDocumentPickerDelegate
