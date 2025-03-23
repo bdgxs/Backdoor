20aef3dbd6d4a8f6148315beadced28692e51449
bdgxs <jojog1010@hotmail.com>
2025-03-23 17:24:41 -0400

    Update HexEditorViewController.swift

--- iOS/Views/Home/HexEditorViewController.swift
+++ iOS/Views/Home/HexEditorViewController.swift
@@ -1,14 +1,16 @@
 import UIKit
 
 class HexEditorViewController: UIViewController, UITextViewDelegate {
-    private let fileURL: URL
-    private var textView: UITextView!
-    private var toolbar: UIToolbar!
-    private var hasUnsavedChanges = false
-    private var autoSaveTimer: Timer?
+    let fileURL: URL
+    let textView: UITextView
+    let toolbar: UIToolbar
+    var hasUnsavedChanges = false
+    var autoSaveTimer: Timer?
 
     init(fileURL: URL) {
         self.fileURL = fileURL
+        self.textView = UITextView()
+        self.toolbar = UIToolbar()
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -25,26 +27,22 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        stopAutoSaveTimer()
         if hasUnsavedChanges {
             promptSaveChanges()
         } else {
             navigationController?.popViewController(animated: true)
         }
-        stopAutoSaveTimer()
     }
 
-    private func setupUI() {
+    func setupUI() {
         view.backgroundColor = .systemBackground
 
-        // Setup text view
-        textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.font = UIFont.monospacedSystemFont(ofSize: 14, weight: .regular)
         textView.delegate = self
         view.addSubview(textView)
 
-        // Setup toolbar
-        toolbar = UIToolbar()
         toolbar.translatesAutoresizingMaskIntoConstraints = false
         let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveChanges))
         let copyButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(copyContent))
@@ -54,7 +52,6 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
         toolbar.items = [saveButton, copyButton, findReplaceButton, undoButton, redoButton, UIBarButtonItem.flexibleSpace()]
         view.addSubview(toolbar)
 
-        // Setup constraints
         NSLayoutConstraint.activate([
             textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
@@ -64,11 +61,15 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
             toolbar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             toolbar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+
+        textView.isAccessibilityElement = true
+        textView.accessibilityLabel = "Hex Editor"
+        toolbar.isAccessibilityElement = true
+        toolbar.accessibilityLabel = "Toolbar"
     }
 
-    private func loadFileContent() {
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            guard let self = self else { return }
+    func loadFileContent() {
+        DispatchQueue.global(qos: .userInitiated).async {
             do {
                 let data = try Data(contentsOf: self.fileURL)
                 let hexString = data.map { String(format: "%02x", $0) }.joined(separator: " ")
@@ -83,29 +84,27 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
         }
     }
 
-    @objc private func saveChanges() {
-        guard let text = textView.text else {
-            return
-        }
+    @objc func saveChanges() {
+        guard let text = textView.text else { return }
         let hexValues = text.components(separatedBy: " ").compactMap { UInt8($0, radix: 16) }
         let data = Data(hexValues)
         do {
             try data.write(to: fileURL)
             hasUnsavedChanges = false
+            HapticFeedbackGenerator.generateNotificationFeedback(type: .success)
             print("File saved successfully.")
         } catch {
-            print("Error saving file: \(error)")
-            presentAlert(title: "Error", message: "Could not save file.")
+            presentAlert(title: "Error", message: "Could not save file: \(error.localizedDescription)")
         }
     }
 
-    @objc private func copyContent() {
+    @objc func copyContent() {
         UIPasteboard.general.string = textView.text
         HapticFeedbackGenerator.generateNotificationFeedback(type: .success)
         print("Content copied to clipboard.")
     }
 
-    @objc private func promptFindReplace() {
+    @objc func promptFindReplace() {
         let alert = UIAlertController(title: "Find and Replace", message: nil, preferredStyle: .alert)
         alert.addTextField { textField in
             textField.placeholder = "Find"
@@ -113,24 +112,22 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
         alert.addTextField { textField in
             textField.placeholder = "Replace"
         }
-        alert.addAction(UIAlertAction(title: "Replace", style: .default, handler: { [weak self] _ in
-            guard let findText = alert.textFields?[0].text, let replaceText = alert.textFields?[1].text else {
-                return
-            }
-            if let currentText = self?.textView.text {
-                self?.textView.text = currentText.replacingOccurrences(of: findText, with: replaceText)
-                self?.hasUnsavedChanges = true
+        alert.addAction(UIAlertAction(title: "Replace", style: .default, handler: { _ in
+            guard let findText = alert.textFields?[0].text, let replaceText = alert.textFields?[1].text else { return }
+            if let currentText = self.textView.text {
+                self.textView.text = currentText.replacingOccurrences(of: findText, with: replaceText)
+                self.hasUnsavedChanges = true
             }
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
-    @objc private func undoAction() {
+    @objc func undoAction() {
         textView.undoManager?.undo()
     }
 
-    @objc private func redoAction() {
+    @objc func redoAction() {
         textView.undoManager?.redo()
     }
 
@@ -138,28 +135,28 @@ class HexEditorViewController: UIViewController, UITextViewDelegate {
         hasUnsavedChanges = true
     }
 
-    private func presentAlert(title: String, message: String) {
+    func presentAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
-    private func startAutoSaveTimer() {
+    func startAutoSaveTimer() {
         autoSaveTimer = Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(autoSave), userInfo: nil, repeats: true)
     }
 
-    private func stopAutoSaveTimer() {
+    func stopAutoSaveTimer() {
         autoSaveTimer?.invalidate()
         autoSaveTimer = nil
     }
 
-    @objc private func autoSave() {
+    @objc func autoSave() {
         if hasUnsavedChanges {
             saveChanges()
         }
     }
 
-    private func promptSaveChanges() {
+    func promptSaveChanges() {
         let alert = UIAlertController(title: "Unsaved Changes", message: "You have unsaved changes. Do you want to save them?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { _ in
             self.saveChanges()
