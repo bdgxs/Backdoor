edadf85169ea5f72827c36e93386e54c8cdf4ee5
bdgxs <jojog1010@hotmail.com>
2025-03-24 19:06:55 -0400

    Update IPAEditorViewController.swift

--- iOS/Views/Home/IPAEditorViewController.swift
+++ iOS/Views/Home/IPAEditorViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import ZIPFoundation
 
-class IPAEditorViewController: UIViewController {
+class IPAEditorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     private let fileURL: URL
     private let tableView: UITableView
     private var contents: [URL] = []
@@ -38,4 +38,54 @@ class IPAEditorViewController: UIViewController {
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            tableView.bottomAnchor.constraint(equal ​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+        
+        tableView.isAccessibilityElement = true
+        tableView.accessibilityLabel = "IPA Contents"
+    }
+    
+    private func extractIPAContents() {
+        let tempDirectory = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        do {
+            try FileManager.default.createDirectory(at: tempDirectory, withIntermediateDirectories: true, attributes: nil)
+            try FileManager.default.unzipItem(at: fileURL, to: tempDirectory)
+            contents = try FileManager.default.contentsOfDirectory(at: tempDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            presentAlert(title: "Error", message: "Failed to extract IPA: \(error.localizedDescription)")
+        }
+    }
+    
+    private func presentAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = contents[indexPath.row].lastPathComponent
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedURL = contents[indexPath.row]
+        if selectedURL.pathExtension.lowercased() == "plist" {
+            let editor = PlistEditorViewController(fileURL: selectedURL)
+            navigationController?.pushViewController(editor, animated: true)
+        } else {
+            presentAlert(title: "Unsupported", message: "Only .plist files can be edited within IPA.")
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
