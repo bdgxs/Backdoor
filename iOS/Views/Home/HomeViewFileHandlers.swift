81134fadda8a9bd7db29cdef275f0defa824f741
bdgxs <jojog1010@hotmail.com>
2025-03-24 22:07:06 -0400

    Update HomeViewFileHandlers.swift

--- iOS/Views/Home/HomeViewFileHandlers.swift
+++ iOS/Views/Home/HomeViewFileHandlers.swift
@@ -15,17 +15,17 @@ class HomeViewFileHandlers {
     private let logger = Logger(subsystem: "com.example.FileNexus", category: "FileHandlers")
     
     func uploadFile(viewController: FileHandlingDelegate) {
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data, .archive, .text])
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data, .archive, .text]) // Removed qualityOfService
         documentPicker.delegate = viewController as? UIDocumentPickerDelegate
         documentPicker.modalPresentationStyle = .formSheet
-        viewController.present(documentPicker, animated: true, completion: nil)
+        viewController.present(documentPicker, animated: true)
     }
     
     func importFile(viewController: FileHandlingDelegate) {
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data, .archive, .text])
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.data, .archive, .text]) // Removed qualityOfService
         documentPicker.delegate = viewController as? UIDocumentPickerDelegate
         documentPicker.modalPresentationStyle = .formSheet
-        viewController.present(documentPicker, animated: true, completion: nil)
+        viewController.present(documentPicker, animated: true)
     }
     
     func createNewFolder(viewController: FileHandlingDelegate, folderName: String, completion: @escaping (Result<URL, Error>) -> Void) {
@@ -89,6 +89,6 @@ class HomeViewFileHandlers {
     func shareFile(viewController: UIViewController, fileURL: URL) {
         let activityController = UIActivityViewController(activityItems: [fileURL], applicationActivities: nil)
         activityController.popoverPresentationController?.sourceView = viewController.view
-        viewController.present(activityController, animated: true, completion: nil)
+        viewController.present(activityController, animated: true)
     }
 }
