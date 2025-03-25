c7f7cb1906d83df725bab32a5edfa2e4ffac1d5b
bdgxs <jojog1010@hotmail.com>
2025-03-25 07:17:38 -0400

    Update Storage.swift

--- Shared/Data/UserDefaults/Storage.swift
+++ Shared/Data/UserDefaults/Storage.swift
@@ -1,92 +1,81 @@
-//
-//  Storage.swift
-//  feather
-//
-//  Created by samara on 5/17/24.
-//  Copyright (c) 2024 Samara M (khcrysalis)
-//
-
 import Foundation
 
+// Renamed to avoid ambiguity with CodableStorage
 @propertyWrapper
-struct Storage<Value> {
-	typealias Callback = (Value) -> Void
-	let key: String
-	let defaultValue: Value
-	let callback: Callback?
-	
-	init(key: String, defaultValue: Value, callback: Callback? = nil) {
-		self.key = key
-		self.defaultValue = defaultValue
-		self.callback = callback
-	}
-	
-	var wrappedValue: Value {
-		get {
-			if let storedValue = UserDefaults.standard.object(forKey: key) {
-				if let castedValue = storedValue as? Value {
-					return castedValue
-				}
-			}
-			return defaultValue
-		}
-		set {
-			UserDefaults.standard.set(newValue, forKey: key)
-			callback?(newValue)
-		}
-	}
-
+struct UserDefaultsStorage<Value> {
+    typealias Callback = (Value) -> Void
+    let key: String
+    let defaultValue: Value
+    let callback: Callback?
+    
+    init(key: String, defaultValue: Value, callback: Callback? = nil) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.callback = callback
+    }
+    
+    var wrappedValue: Value {
+        get {
+            if let storedValue = UserDefaults.standard.object(forKey: key) as? Value {
+                return storedValue
+            }
+            return defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+            callback?(newValue)
+        }
+    }
 }
 
-
+// Explicitly named to avoid redeclaration conflicts
 @propertyWrapper
-public struct CodableStorage<Value: Codable> {
-	public typealias Handler = (String, Value) -> Void
-	
-	var key: String
-	var defaultValue: Value
-	var handler: Handler? = nil
-	
-	public init(key: String, defaultValue: Value, handler: Handler? = nil) {
-		self.key = key
-		self.defaultValue = defaultValue
-		self.handler = handler
-	}
-	
-	public var wrappedValue: Value {
-		get {
-			guard let data = UserDefaults.standard.data(forKey: key) else {
-				return defaultValue
-			}
-			do {
-				let decoded = try decoder.decode(Value.self, from: data)
-				return decoded
-			} catch {
-				Debug.shared.log(message: "Decoding \(Value.self) failed. \(error)")
-			}
-			return defaultValue
-		}
-		
-		set {
-			do {
-				let newData = try encoder.encode(newValue)
-				UserDefaults.standard.set(newData, forKey: key)
-				handler?(key, newValue)
-			} catch {
-				Debug.shared.log(message: "\(error)")
-			}
-		}
-	}
+public struct CodableUserDefaultsStorage<Value: Codable> {
+    public typealias Handler = (String, Value) -> Void
+    
+    private let key: String
+    private let defaultValue: Value
+    private var handler: Handler?
+    
+    public init(key: String, defaultValue: Value, handler: Handler? = nil) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.handler = handler
+    }
+    
+    public var wrappedValue: Value {
+        get {
+            guard let data = UserDefaults.standard.data(forKey: key) else {
+                return defaultValue
+            }
+            do {
+                return try decoder.decode(Value.self, from: data)
+            } catch {
+                Debug.shared.log(message: "Decoding \(Value.self) failed. \(error)")
+                return defaultValue
+            }
+        }
+        
+        set {
+            do {
+                let newData = try encoder.encode(newValue)
+                UserDefaults.standard.set(newData, forKey: key)
+                handler?(key, newValue)
+            } catch {
+                Debug.shared.log(message: "Encoding failed: \(error)")
+            }
+        }
+    }
 }
 
 public let encoder: JSONEncoder = {
-	let enc = JSONEncoder()
-	enc.dateEncodingStrategy = .iso8601
-	return enc
+    let enc = JSONEncoder()
+    enc.dateEncodingStrategy = .iso8601
+    return enc
 }()
 
 public let decoder: JSONDecoder = {
-	let dec = JSONDecoder()
-	dec.dateDecodingStrategy = .iso8601
-	return dec
+    let dec = JSONDecoder()
+    dec.dateDecodingStrategy = .iso8601
+    return dec
 }()
