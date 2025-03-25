05fdf38f2470a215bce16032866affc2236e357f
bdgxs <jojog1010@hotmail.com>
2025-03-25 02:54:38 -0400

    Create UIApplication+TopViewController.swift

---
+++ iOS/Extensions/UIApplication+TopViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension UIApplication {
+    func topMostViewController() -> UIViewController? {
+        guard let windowScene = connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first else {
+            return nil
+        }
+        var topController = window.rootViewController
+        while let presentedController = topController?.presentedViewController {
+            topController = presentedController
+        }
+        return topController
+    }
+}
+
+extension Notification.Name {
+    static let showAIAssistant = Notification.Name("showAIAssistant")
+}
