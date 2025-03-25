482d38d6c0d0eb648f1d1950655bb7be9bbe4bdf
bdgxs <jojog1010@hotmail.com>
2025-03-25 06:00:36 -0400

    Update Preferences.swift

--- Shared/Data/UserDefaults/Preferences.swift
+++ Shared/Data/UserDefaults/Preferences.swift
@@ -1,106 +1,135 @@
-//
-//  Preferences.swift
-//  feather
-//
-//  Created by samara on 5/17/24.
-//  Copyright (c) 2024 Samara M (khcrysalis)
-//
-
 import Foundation
 import UIKit
 
-enum Preferences {
-	static var installPathChangedCallback: ((String?) -> Void)?
-	static let defaultInstallPath: String = "https://api.palera.in"
-	
-	@Storage(key: "Feather.UserSpecifiedOnlinePath", defaultValue: defaultInstallPath)
-	static var onlinePath: String? { didSet { installPathChangedCallback?(onlinePath) } }
-	
-	@Storage(key: "Feather.UserSelectedServer", defaultValue: false)
-	static var userSelectedServer: Bool
-	
-	@Storage(key: "Feather.DefaultRepos", defaultValue: false)
-	// Default repo is from the repository
-	static var defaultRepos: Bool
-	
-	@Storage(key: "Feather.AppUpdates", defaultValue: false)
-	// Default repo is from the repository
-	static var appUpdates: Bool
-	
-	@Storage(key: "Feather.gotSSLCerts", defaultValue: false)
-	static var gotSSLCerts: Bool
-	
-	@Storage(key: "Feather.BDefaultRepos", defaultValue: false)
-	// Default beta repo is from the repository
-	static var bDefaultRepos: Bool
-	
-	@Storage(key: "Feather.userIntefacerStyle", defaultValue: UIUserInterfaceStyle.unspecified.rawValue)
-	static var preferredInterfaceStyle: Int
-	
-	@CodableStorage(key: "Feather.AppTintColor", defaultValue: CodableColor(UIColor(hex: "FFA500")))
-	static var appTintColor: CodableColor
-	
-	@Storage(key: "Feather.OnboardingActive", defaultValue: true)
-	static var isOnboardingActive: Bool
-	
-	@Storage(key: "Feather.selectedCert", defaultValue: 0)
-	static var selectedCert: Int
-	
-	@Storage(key: "Feather.ppqcheckBypass", defaultValue: "")
-	// random string
-	static var pPQCheckString: String
-	
-	@Storage(key: "Feather.CertificateTitleAppIDtoTeamID", defaultValue: false)
-	static var certificateTitleAppIDtoTeamID: Bool
-	
-	@Storage(key: "Feather.AppDescriptionAppearence", defaultValue: 0)
-	// 0 == Default appearence
-	// 1 == Replace subtitle with localizedDescription
-	// 2 == Move localizedDescription below app icon, and above screenshots
-	static var appDescriptionAppearence: Int
-	
-	@Storage(key: "UserPreferredLanguageCode", defaultValue: nil, callback: preferredLangChangedCallback)
-	/// Preferred language
-	static var preferredLanguageCode: String?
-	
-	@Storage(key: "Feather.Beta", defaultValue: false)
-	//
-	static var beta: Bool
-	
-	@CodableStorage(key: "SortOption", defaultValue: SortOption.default)
-	static var currentSortOption: SortOption
-	
-	@Storage(key: "SortOptionAscending", defaultValue: true)
-	static var currentSortOptionAscending: Bool
+struct SigningOptions: Codable, CustomStringConvertible {
+    var ppqCheckProtection: Bool = false
+    var dynamicProtection: Bool = false
+    var installAfterSigned: Bool = false
+    var immediatelyInstallFromSource: Bool = false
+    var removePlugins: Bool = false
+    var forceFileSharing: Bool = false
+    var removeSupportedDevices: Bool = false
+    var removeURLScheme: Bool = false
+    var forceProMotion: Bool = false
+    var forceGameMode: Bool = false
+    var forceForceFullScreen: Bool = false
+    var forceiTunesFileSharing: Bool = false
+    var forceTryToLocalize: Bool = false
+    var removeProvisioningFile: Bool = false
+    var removeWatchPlaceHolder: Bool = false
+    var bundleIdConfig: [String: String] = [:]
+    var displayNameConfig: [String: String] = [:]
+
+    var description: String {
+        return """
+        PPQ Check: \(ppqCheckProtection), Dynamic: \(dynamicProtection), Install After Signed: \(installAfterSigned),
+        Immediately Install: \(immediatelyInstallFromSource), Remove Plugins: \(removePlugins),
+        Force File Sharing: \(forceFileSharing), Remove Supported Devices: \(removeSupportedDevices),
+        Remove URL Scheme: \(removeURLScheme), Force ProMotion: \(forceProMotion),
+        Force Game Mode: \(forceGameMode), Force Fullscreen: \(forceForceFullScreen),
+        Force iTunes Sharing: \(forceiTunesFileSharing), Force Localize: \(forceTryToLocalize),
+        Remove Provisioning: \(removeProvisioningFile), Remove Watch Placeholder: \(removeWatchPlaceHolder)
+        """
+    }
 }
 
-// MARK: - Callbacks
-fileprivate extension Preferences {
-	static func preferredLangChangedCallback(newValue: String?) {
-		Bundle.preferredLocalizationBundle = .makeLocalizationBundle(preferredLanguageCode: newValue)
-	}
+enum Preferences {
+    @Storage(key: "Feather.AppTintColor", defaultValue: CodableColor(.systemBlue))
+    static var appTintColor: CodableColor
+
+    @Storage(key: "Feather.PreferredInterfaceStyle", defaultValue: 0)
+    static var preferredInterfaceStyle: Int
+
+    @Storage(key: "Feather.IsOnboardingActive", defaultValue: true)
+    static var isOnboardingActive: Bool
+
+    @Storage(key: "Feather.AppUpdates", defaultValue: true)
+    static var appUpdates: Bool
+
+    @Storage(key: "Feather.DefaultRepos", defaultValue: true)
+    static var defaultRepos: Bool
+
+    @Storage(key: "Feather.GotSSLCerts", defaultValue: false)
+    static var gotSSLCerts: Bool
+
+    @Storage(key: "Feather.PPQCheckString", defaultValue: "")
+    static var pPQCheckString: String
+
+    @CodableStorage(key: "Feather.SigningOptions", defaultValue: SigningOptions())
+    static var signingOptions: SigningOptions
 }
-// MARK: - Color
+
+// Storage Property Wrapper
+@propertyWrapper
+struct Storage<T> {
+    private let key: String
+    private let defaultValue: T
+
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get { UserDefaults.standard.object(forKey: key) as? T ?? defaultValue }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+}
+
+// Codable Storage Property Wrapper
+@propertyWrapper
+struct CodableStorage<T: Codable> {
+    private let key: String
+    private let defaultValue: T
+
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            guard let data = UserDefaults.standard.data(forKey: key),
+                  let value = try? JSONDecoder().decode(T.self, from: data) else {
+                return defaultValue
+            }
+            return value
+        }
+        set {
+            if let data = try? JSONEncoder().encode(newValue) {
+                UserDefaults.standard.set(data, forKey: key)
+            }
+        }
+    }
+}
 
 struct CodableColor: Codable {
-	let red: CGFloat
-	let green: CGFloat
-	let blue: CGFloat
-	let alpha: CGFloat
-	
-	var uiColor: UIColor {
-		return UIColor(red: self.red, green: self.green, blue: self.blue, alpha: self.alpha)
-	}
-	
-	init(_ color: UIColor) {
-		var _red: CGFloat = 0, _green: CGFloat = 0, _blue: CGFloat = 0, _alpha: CGFloat = 0
-		
-		color.getRed(&_red, green: &_green, blue: &_blue, alpha: &_alpha)
-		
-		self.red = _red
-		self.blue = _blue
-		self.green = _green
-		self.alpha = _alpha
-	}
-}
+    let uiColor: UIColor
 
+    init(_ color: UIColor) {
+        self.uiColor = color
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case red, green, blue, alpha
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let r = try container.decode(CGFloat.self, forKey: .red)
+        let g = try container.decode(CGFloat.self, forKey: .green)
+        let b = try container.decode(CGFloat.self, forKey: .blue)
+        let a = try container.decode(CGFloat.self, forKey: .alpha)
+        self.uiColor = UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        uiColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        try container.encode(r, forKey: .red)
+        try container.encode(g, forKey: .green)
+        try container.encode(b, forKey: .blue)
+        try container.encode(a, forKey: .alpha)
+    }
+}
