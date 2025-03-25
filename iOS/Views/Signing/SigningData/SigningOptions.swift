4d8cc1d8c4258fa340cc2873f68f0c9d1b5af6bb
bdgxs <jojog1010@hotmail.com>
2025-03-25 08:28:52 -0400

    Update SigningOptions.swift

--- iOS/Views/Signing/SigningData/SigningOptions.swift
+++ iOS/Views/Signing/SigningData/SigningOptions.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import UIKit
 
-//enum Orientation {
+// enum Orientation {
 //	.top
 //	.bottom
 //	.left
@@ -15,70 +16,41 @@ import Foundation
 //}
 
 struct MainSigningOptions {
-	var name: String?
-	var version: String?
-	var bundleId: String?
-	var iconURL: UIImage?
+    var name: String?
+    var version: String?
+    var bundleId: String?
+    var iconURL: UIImage?
 
-	var uuid: String?
-	var removeInjectPaths: [String] = []
-	
-	let forceMinimumVersionString = ["Automatic", "15.0", "14.0", "13.0"]
-	let forceLightDarkAppearenceString = ["Automatic", "Light", "Dark"]
-	
-	var certificate: Certificate?
-}
-
-struct SigningOptions: Codable {
-	var ppqCheckProtection: Bool = false
-	var dynamicProtection: Bool = false
-	var installAfterSigned: Bool = false
-	var immediatelyInstallFromSource: Bool = false
-	
-	var bundleIdConfig: [String: String] = [:]
-	var displayNameConfig: [String: String] = [:]
-	var toInject: [String] = []
-	
-	var removePlugins: Bool = false
-	var forceFileSharing: Bool = true
-	var removeSupportedDevices: Bool = true
-	var removeURLScheme: Bool = false
-	var forceProMotion: Bool = false
-	var forceGameMode: Bool = false
-	
-	var forceForceFullScreen: Bool = false
-	var forceiTunesFileSharing: Bool = true
-	var forceTryToLocalize: Bool = false
-	
-	var removeProvisioningFile: Bool = true
-	var removeWatchPlaceHolder: Bool = true
-	
-	var forceMinimumVersion: String = "Automatic"
-	var forceLightDarkAppearence: String = "Automatic"
+    var uuid: String?
+    var removeInjectPaths: [String] = []
+    
+    let forceMinimumVersionString = ["Automatic", "15.0", "14.0", "13.0"]
+    let forceLightDarkAppearenceString = ["Automatic", "Light", "Dark"]
+    
+    var certificate: Certificate?
 }
 
 extension UserDefaults {
-	static let signingDataKey = "defaultSigningData"
-	
-	static let defaultSigningData = SigningOptions()
-	
-	var signingOptions: SigningOptions {
-		get {
-			if let data = data(forKey: UserDefaults.signingDataKey),
-			   let options = try? JSONDecoder().decode(SigningOptions.self, from: data) {
-				return options
-			}
-			return UserDefaults.defaultSigningData
-		}
-		set {
-			if let data = try? JSONEncoder().encode(newValue) {
-				set(data, forKey: UserDefaults.signingDataKey)
-			}
-		}
-	}
-	
-	func resetSigningOptions() {
-		signingOptions = UserDefaults.defaultSigningData
-	}
+    static let signingDataKey = "defaultSigningData"
+    
+    static let defaultSigningData = SigningOptions() // References the consolidated SigningOptions from Preferences.swift
+    
+    var signingOptions: SigningOptions {
+        get {
+            if let data = data(forKey: UserDefaults.signingDataKey),
+               let options = try? JSONDecoder().decode(SigningOptions.self, from: data) {
+                return options
+            }
+            return UserDefaults.defaultSigningData
+        }
+        set {
+            if let data = try? JSONEncoder().encode(newValue) {
+                set(data, forKey: UserDefaults.signingDataKey)
+            }
+        }
+    }
+    
+    func resetSigningOptions() {
+        signingOptions = UserDefaults.defaultSigningData
+    }
 }
-
