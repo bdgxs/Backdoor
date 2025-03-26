5e8cf654c8fd749c326f91912e29411ea348f2c1
bdgxs <jojog1010@hotmail.com>
2025-03-26 00:14:13 -0400

    Update CoreDataManager+Certificates.swift

--- Shared/Data/CoreData/CoreDataManager+Certificates.swift
+++ Shared/Data/CoreData/CoreDataManager+Certificates.swift
@@ -3,8 +3,7 @@ import CoreData
 
 extension CoreDataManager {
     /// clear certificates data
-    func clearCertificate(
-        context: NSManagedObjectContext? = nil) {
+    func clearCertificate(context: NSManagedObjectContext? = nil) {
         let context = context ?? self.context
         do {
             try clear(request: Certificate.fetchRequest(), context: context)
@@ -12,16 +11,14 @@ extension CoreDataManager {
             Debug.shared.log(message: "Error clearing certificates: \(error)", type: .error)
         }
     }
-    
-    func getDatedCertificate(
-        context: NSManagedObjectContext? = nil) -> [Certificate] {
+
+    func getDatedCertificate(context: NSManagedObjectContext? = nil) -> [Certificate] {
         let request: NSFetchRequest<Certificate> = Certificate.fetchRequest()
         request.sortDescriptors = [NSSortDescriptor(key: "dateAdded", ascending: true)]
         return (try? (context ?? self.context).fetch(request)) ?? []
     }
-    
-    func getCurrentCertificate(
-        context: NSManagedObjectContext? = nil) -> Certificate? {
+
+    func getCurrentCertificate(context: NSManagedObjectContext? = nil) -> Certificate? {
         let context = context ?? self.context
         let row = Preferences.selectedCert
         let certificates = getDatedCertificate(context: context)
@@ -31,22 +28,22 @@ extension CoreDataManager {
             return nil
         }
     }
-    
+
     func addToCertificates(cert: Cert, files: [CertImportingViewController.FileType: Any], context: NSManagedObjectContext? = nil) {
         let context = context ?? self.context
-        
+
         guard let provisionPath = files[.provision] as? URL else {
             Debug.shared.log(message: "Provisioning file URL is missing")
             return
         }
-        
+
         let p12Path = files[.p12] as? URL
         let uuid = UUID().uuidString
-        
+
         let newCertificate = createCertificateEntity(uuid: uuid, provisionPath: provisionPath, p12Path: p12Path, password: files[.password] as? String, context: context)
         let certData = createCertificateDataEntity(cert: cert, context: context)
         newCertificate.certData = certData
-        
+
         do {
             try saveCertificateFiles(uuid: uuid, provisionPath: provisionPath, p12Path: p12Path)
             try context.save()
@@ -55,7 +52,7 @@ extension CoreDataManager {
             Debug.shared.log(message: "Error: \(error)", type: .error)
         }
     }
-    
+
     private func createCertificateEntity(uuid: String, provisionPath: URL, p12Path: URL?, password: String?, context: NSManagedObjectContext) -> Certificate {
         let newCertificate = Certificate(context: context)
         newCertificate.uuid = uuid
@@ -65,7 +62,7 @@ extension CoreDataManager {
         newCertificate.password = password
         return newCertificate
     }
-    
+
     private func createCertificateDataEntity(cert: Cert, context: NSManagedObjectContext) -> CertificateData {
         let certData = CertificateData(context: context)
         certData.appIDName = cert.AppIDName
@@ -79,28 +76,28 @@ extension CoreDataManager {
         certData.version = Int32(cert.Version)
         return certData
     }
-    
+
     private func saveCertificateFiles(uuid: String, provisionPath: URL, p12Path: URL?) throws {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let destinationDirectory = documentsDirectory
             .appendingPathComponent("Certificates")
             .appendingPathComponent(uuid)
-        
+
         try FileManager.default.createDirectory(at: destinationDirectory, withIntermediateDirectories: true, attributes: nil)
         try CertData.copyFile(from: provisionPath, to: destinationDirectory)
         try CertData.copyFile(from: p12Path, to: destinationDirectory)
     }
-    
+
     func getCertifcatePath(source: Certificate?) throws -> URL {
         guard let source, let uuid = source.uuid else { throw FileProcessingError.missingFile("Certificate or UUID") }
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let destinationDirectory = documentsDirectory
             .appendingPathComponent("Certificates")
             .appendingPathComponent(uuid)
-        
+
         return destinationDirectory
     }
-    
+
     func deleteAllCertificateContent(for app: Certificate) {
         do {
             CoreDataManager.shared.context.delete(app)
