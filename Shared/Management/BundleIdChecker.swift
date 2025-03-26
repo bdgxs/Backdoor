a1ede3b5e1b81e5a639571149d1bad4c788426bb
bdgxs <jojog1010@hotmail.com>
2025-03-26 02:32:18 -0400

    Update BundleIdChecker.swift

--- Shared/Management/BundleIdChecker.swift
+++ Shared/Management/BundleIdChecker.swift
@@ -4,11 +4,11 @@ class BundleIdChecker {
     static func shouldModifyBundleId(originalBundleId: String) async -> Bool {
         do {
             let exists = try await iTunesLookup.checkBundleId(originalBundleId)
-            Debug.shared.log(message: "Dynamic Protection: Bundle ID \(originalBundleId) exists on App Store: \(exists)", type: .info)
+            Logger.shared.log(message: "Dynamic Protection: Bundle ID \(originalBundleId) exists on App Store: \(exists)", type: .info)
             return exists
         } catch {
-            Debug.shared.log(message: "Dynamic Protection: Failed to check bundle ID \(originalBundleId), applying protection as precaution: \(error.localizedDescription)", type: .error)
+            Logger.shared.log(message: "Dynamic Protection: Failed to check bundle ID \(originalBundleId), applying protection as precaution: \(error.localizedDescription)", type: .error)
             return true
         }
     }
-} 
+}
