1b9dc723798bd7bb4d627ab974f34a3714c8d1a0
bdgxs <jojog1010@hotmail.com>
2025-03-26 04:04:09 -0400

    Update main.swift

--- iOS/Delegates/main.swift
+++ iOS/Delegates/main.swift
@@ -1,11 +1,8 @@
-//
-//  main.swift
-//  feather
-//
-//  Created by samara on 5/17/24.
-//
-
 import UIKit
-import func Foundation.NSStringFromClass
 
-_ = UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
