133001826f77fb0f9d83ee03f9f2b7b3a024427e
bdgxs <jojog1010@hotmail.com>
2025-03-26 03:49:45 -0400

    Update ResetDataClass.swift

--- iOS/Views/Settings/Reset/ResetDataClass.swift
+++ iOS/Views/Settings/Reset/ResetDataClass.swift
@@ -80,3 +80,8 @@ class ResetDataClass {
 		}
 	}
 }
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
